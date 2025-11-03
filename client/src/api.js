c5644dbc56989fc1e93efa2901fb688469b0b38f
PusuluriSwathi <pusuluriswathi276@gmail.com>
2025-11-03 19:20:27 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const base = process.env.REACT_APP_API_URL || '/api';
+const base = `${process.env.REACT_APP_API_URL}/api`;
 
 const api = axios.create({
   baseURL: base,

24e36412d4170c14faaa2574f94f4d430ee4437f
PusuluriSwathi <pusuluriswathi276@gmail.com>
2025-11-03 14:47:52 +0530

    Add route to get a single student by ID

--- server/routes/StudentRoutes.js
+++ server/routes/StudentRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { createStudent, getStudents, updateStudent, deleteStudent } = require('../controllers/StudentController');
+const {
+  createStudent,
+  getStudents,
+  getStudent, // <- added
+  updateStudent,
+  deleteStudent,
+} = require('../controllers/StudentController');
+
 const router = express.Router();
 
 router.post('/students', createStudent);
 router.get('/students', getStudents);
+router.get('/students/:id', getStudent); // <- added single-item route
 router.put('/students/:id', updateStudent);
 router.delete('/students/:id', deleteStudent);
 
